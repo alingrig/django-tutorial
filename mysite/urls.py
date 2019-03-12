791d7dcb7150616cee449d6604f0457b86a5cabc
Alin <alin.grig93@gmail.com>
2019-03-12 17:20:12 +0100

    add html code for cern auth

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import url
 
 urlpatterns = [
     path('polls/',include('polls.urls')),
     path('admin/', admin.site.urls),
-    path(r'^accounts/', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
 

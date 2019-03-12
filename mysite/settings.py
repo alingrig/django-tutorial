046605798fd59474c599bfa31341b94603a15e9a
Alin <alin.grig93@gmail.com>
2019-03-12 16:43:12 +0100

    add cern auth

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,19 +25,99 @@ SECRET_KEY = 't^9g6iljs8#r4^f=z7k*(inp-$9skpvrbw%1au4=&gyy&l^zs!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['test-django-2.web.cern.ch']
-
+ALLOWED_HOSTS = ['test-django-2.web.cern.ch','127.0.0.1']
 
 # Application definition
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
     'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # The following apps are required:
+    'django.contrib.auth',
+    'django.contrib.messages',
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    # ... include the providers you want to enable:
+    'allauth.socialaccount.providers.agave',
+    'allauth.socialaccount.providers.amazon',
+    'allauth.socialaccount.providers.angellist',
+    'allauth.socialaccount.providers.asana',
+    'allauth.socialaccount.providers.auth0',
+    'allauth.socialaccount.providers.authentiq',
+    'allauth.socialaccount.providers.baidu',
+    'allauth.socialaccount.providers.basecamp',
+    'allauth.socialaccount.providers.bitbucket',
+    'allauth.socialaccount.providers.bitbucket_oauth2',
+    'allauth.socialaccount.providers.bitly',
+    'allauth.socialaccount.providers.cern',
+    'allauth.socialaccount.providers.coinbase',
+    'allauth.socialaccount.providers.dataporten',
+    'allauth.socialaccount.providers.daum',
+    'allauth.socialaccount.providers.digitalocean',
+    'allauth.socialaccount.providers.discord',
+    'allauth.socialaccount.providers.disqus',
+    'allauth.socialaccount.providers.douban',
+    'allauth.socialaccount.providers.draugiem',
+    'allauth.socialaccount.providers.dropbox',
+    'allauth.socialaccount.providers.dwolla',
+    'allauth.socialaccount.providers.edmodo',
+    'allauth.socialaccount.providers.eveonline',
+    'allauth.socialaccount.providers.evernote',
+    'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.feedly',
+    'allauth.socialaccount.providers.fivehundredpx',
+    'allauth.socialaccount.providers.flickr',
+    'allauth.socialaccount.providers.foursquare',
+    'allauth.socialaccount.providers.fxa',
+    'allauth.socialaccount.providers.github',
+    'allauth.socialaccount.providers.gitlab',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.hubic',
+    'allauth.socialaccount.providers.instagram',
+    'allauth.socialaccount.providers.jupyterhub',
+    'allauth.socialaccount.providers.kakao',
+    'allauth.socialaccount.providers.line',
+    'allauth.socialaccount.providers.linkedin',
+    'allauth.socialaccount.providers.linkedin_oauth2',
+    'allauth.socialaccount.providers.mailru',
+    'allauth.socialaccount.providers.mailchimp',
+    'allauth.socialaccount.providers.meetup',
+    'allauth.socialaccount.providers.naver',
+    'allauth.socialaccount.providers.odnoklassniki',
+    'allauth.socialaccount.providers.openid',
+    'allauth.socialaccount.providers.orcid',
+    'allauth.socialaccount.providers.paypal',
+    'allauth.socialaccount.providers.persona',
+    'allauth.socialaccount.providers.pinterest',
+    'allauth.socialaccount.providers.reddit',
+    'allauth.socialaccount.providers.robinhood',
+    'allauth.socialaccount.providers.shopify',
+    'allauth.socialaccount.providers.slack',
+    'allauth.socialaccount.providers.soundcloud',
+    'allauth.socialaccount.providers.spotify',
+    'allauth.socialaccount.providers.stackexchange',
+    'allauth.socialaccount.providers.steam',
+    'allauth.socialaccount.providers.stripe',
+    'allauth.socialaccount.providers.trello',
+    'allauth.socialaccount.providers.tumblr',
+    'allauth.socialaccount.providers.twentythreeandme',
+    'allauth.socialaccount.providers.twitch',
+    'allauth.socialaccount.providers.twitter',
+    'allauth.socialaccount.providers.untappd',
+    'allauth.socialaccount.providers.vimeo',
+    'allauth.socialaccount.providers.vimeo_oauth2',
+    'allauth.socialaccount.providers.vk',
+    'allauth.socialaccount.providers.weibo',
+    'allauth.socialaccount.providers.weixin',
+    'allauth.socialaccount.providers.windowslive',
+    'allauth.socialaccount.providers.xing',
 ]
 
 MIDDLEWARE = [
@@ -100,6 +180,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
@@ -114,6 +201,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+SITE_ID = 1
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
